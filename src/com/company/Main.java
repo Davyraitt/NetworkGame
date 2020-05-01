9238b5d621b483296474909a38c5de0afd1c610c
Davy <dj.raitt@student.avans.nl>
2020-05-01 15:41:36 +0200

    Test 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println ("Just a test for the git init :) " );
+        System.out.println ("test2" );
     }
 }
