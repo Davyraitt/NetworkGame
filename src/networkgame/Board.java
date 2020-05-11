d2236d7c2034f2139f15c189711233c200ed7c6f
Davy <dj.raitt@student.avans.nl>
2020-05-11 21:07:53 +0200

    We can move pieces now, mousedrag method implemented

--- src/networkgame/Board.java
+++ src/networkgame/Board.java
@@ -3,6 +3,7 @@ package networkgame;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
@@ -10,6 +11,7 @@ import org.jfree.fx.ResizableCanvas;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
 
 public class Board extends Application {
 	
@@ -30,6 +32,36 @@ public class Board extends Application {
 	private int lastFPSValue = 0;
 	private boolean colored = false;
 	
+	//creating the pieces for player 1;
+	private Piece P1Piece1;
+	private Piece P1Piece2;
+	private Piece P1Piece3;
+	private Piece P1Piece4;
+	private Piece P1Piece5;
+	private Piece P1Piece6;
+	private Piece P1Piece7;
+	private Piece P1Piece8;
+	private Piece P1Piece9;
+	private Piece P1Piece10;
+	private Piece P1Piece11;
+	private Piece P1Piece12;
+	
+	//creating the pieces for player 2;
+	private Piece P2Piece1;
+	private Piece P2Piece2;
+	private Piece P2Piece3;
+	private Piece P2Piece4;
+	private Piece P2Piece5;
+	private Piece P2Piece6;
+	private Piece P2Piece7;
+	private Piece P2Piece8;
+	private Piece P2Piece9;
+	private Piece P2Piece10;
+	private Piece P2Piece11;
+	private Piece P2Piece12;
+	
+	// arraylist with all the pieces
+	ArrayList < Piece > allPieces = new ArrayList <> ( );
 	
 	@Override
 	public void start ( Stage stage ) throws Exception {
@@ -40,6 +72,9 @@ public class Board extends Application {
 		canvas = new ResizableCanvas ( g -> draw ( g ) , mainPane );
 		canvas.setFocusTraversable ( true );
 		mainPane.setCenter ( canvas );
+		
+		canvas.setOnMouseDragged ( this::mouseDragged );
+		
 		FXGraphics2D g2d = new FXGraphics2D ( canvas.getGraphicsContext2D ( ) );
 		
 		//calls our init method
@@ -82,14 +117,83 @@ public class Board extends Application {
 		stage.show ( );
 	}
 	
+	private void mouseDragged ( MouseEvent mouseEvent ) {
+		
+		for ( int i = 0 ; i < allPieces.size ( ) ; i++ )
+		{
+			if ( allPieces.get ( i ).getEllipse2D ( ).contains ( mouseEvent.getX ( ) , mouseEvent.getY ( ) ) )
+			{
+				allPieces.get ( i ).getPiece ( );
+				allPieces.get ( i ).update ( ( int ) mouseEvent.getX ( ) + - 50 , ( int ) mouseEvent.getY ( ) + - 50 );
+			}
+		}
+		
+	}
+	
 	public void init ( ) { // initialisation method
-	
-	
+		
+		//creating the pieces for player 1;
+		P1Piece1 = new Piece ( 2 , 1 , Color.pink , "Player1" , 1 );
+		P1Piece2 = new Piece ( 4 , 1 , Color.pink , "Player1" , 2 );
+		P1Piece3 = new Piece ( 6 , 1 , Color.pink , "Player1" , 3 );
+		P1Piece4 = new Piece ( 8 , 1 , Color.pink , "Player1" , 4 );
+		P1Piece5 = new Piece ( 1 , 2 , Color.pink , "Player1" , 5 );
+		P1Piece6 = new Piece ( 3 , 2 , Color.pink , "Player1" , 6 );
+		P1Piece7 = new Piece ( 5 , 2 , Color.pink , "Player1" , 7 );
+		P1Piece8 = new Piece ( 7 , 2 , Color.pink , "Player1" , 8 );
+		P1Piece9 = new Piece ( 2 , 3 , Color.pink , "Player1" , 9 );
+		P1Piece10 = new Piece ( 4 , 3 , Color.pink , "Player1" , 10 );
+		P1Piece11 = new Piece ( 6 , 3 , Color.pink , "Player1" , 11 );
+		P1Piece12 = new Piece ( 8 , 3 , Color.pink , "Player1" , 12 );
+		
+		allPieces.add ( P1Piece1 );
+		allPieces.add ( P1Piece2 );
+		allPieces.add ( P1Piece3 );
+		allPieces.add ( P1Piece4 );
+		allPieces.add ( P1Piece5 );
+		allPieces.add ( P1Piece6 );
+		allPieces.add ( P1Piece7 );
+		allPieces.add ( P1Piece8 );
+		allPieces.add ( P1Piece9 );
+		allPieces.add ( P1Piece10 );
+		allPieces.add ( P1Piece11 );
+		allPieces.add ( P1Piece12 );
+		
+		//creating the pieces for player 2;
+		
+		P2Piece1 = new Piece ( 1 , 8 , Color.CYAN , "Player2" , 1 );
+		P2Piece2 = new Piece ( 3 , 8 , Color.CYAN , "Player2" , 2 );
+		P2Piece3 = new Piece ( 5 , 8 , Color.CYAN , "Player2" , 3 );
+		P2Piece4 = new Piece ( 7 , 8 , Color.CYAN , "Player2" , 4 );
+		P2Piece5 = new Piece ( 2 , 7 , Color.CYAN , "Player2" , 5 );
+		P2Piece6 = new Piece ( 4 , 7 , Color.CYAN , "Player2" , 6 );
+		P2Piece7 = new Piece ( 6 , 7 , Color.CYAN , "Player2" , 7 );
+		P2Piece8 = new Piece ( 8 , 7 , Color.CYAN , "Player2" , 8 );
+		P2Piece9 = new Piece ( 1 , 6 , Color.CYAN , "Player2" , 9 );
+		P2Piece10 = new Piece ( 3 , 6 , Color.CYAN , "Player2" , 10 );
+		P2Piece11 = new Piece ( 5 , 6 , Color.CYAN , "Player2" , 11 );
+		P2Piece12 = new Piece ( 7 , 6 , Color.CYAN , "Player2" , 12 );
+		
+		allPieces.add ( P2Piece1 );
+		allPieces.add ( P2Piece2 );
+		allPieces.add ( P2Piece3 );
+		allPieces.add ( P2Piece4 );
+		allPieces.add ( P2Piece5 );
+		allPieces.add ( P2Piece6 );
+		allPieces.add ( P2Piece7 );
+		allPieces.add ( P2Piece8 );
+		allPieces.add ( P2Piece9 );
+		allPieces.add ( P2Piece10 );
+		allPieces.add ( P2Piece11 );
+		allPieces.add ( P2Piece12 );
+		
 	}
 	
 	private void update ( double v ) {
 		long now = System.nanoTime ( );
 		long last = -1;
+		
+		
 	}
 	
 	private void draw ( FXGraphics2D graphics ) {
@@ -128,14 +232,16 @@ public class Board extends Application {
 			}
 		}
 		
-		Piece piece1 = new Piece ( 1, 1 );
-		piece1.draw ( graphics );
-		
 		//FPS drawen
 		if ( timer >= 1000000000 )
 		{ // prints how many updates we did in the last second?
 			lastFPSValue = updates;
 			
+		}
+		
+		for ( int i = 0 ; i < allPieces.size ( ) ; i++ )
+		{
+			allPieces.get ( i ).draw ( graphics );
 		}
 		
 		//draws the FPS right corner
@@ -144,4 +250,5 @@ public class Board extends Application {
 		// Stop drawing
 		graphics.dispose ( );
 	}
+	
 }
--- src/networkgame/Piece.java
+++ src/networkgame/Piece.java
@@ -9,14 +9,40 @@ public class Piece {
 	
 	float xPos;
 	float yPos;
+	Color colorPiece;
+	private Ellipse2D ellipse2D;
+	private String player;
+	private int piecenr;
 	
-	public Piece ( float x, float y) {
+	
+	public Piece ( float x, float y, Color color, String playerstring, int number) {
 		xPos = ( x - 1) * 125;
 		yPos = ( y - 1) * 125;
+		ellipse2D =  new Ellipse2D.Double ( (12 + xPos), (10 + yPos) , 100 , 100 );
+		colorPiece = color;
+		player = playerstring;
+		piecenr = number;
+		
 	}
 	
 	public void draw ( FXGraphics2D graphics) {
-		graphics.setColor ( Color.pink );
-		graphics.fill ( new Ellipse2D.Double ( (12 + xPos), (10 + yPos) , 100 , 100 ) );
+		graphics.setColor ( colorPiece );
+		graphics.fill ( ellipse2D );
 	}
+	
+	public void update ( int newxPos, int newyPos ) {
+		ellipse2D =  new Ellipse2D.Double ( (newxPos), (newyPos) , 100 , 100 );
+		
+	}
+	
+	public Ellipse2D getEllipse2D ( ) {
+		return ellipse2D;
+	}
+	
+	
+	public void getPiece ( ) {
+		System.out.println ( "The selected piece is: " + piecenr + "       // from player: " + player);
+	}
+	
+	
 }
