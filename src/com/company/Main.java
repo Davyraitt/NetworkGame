102cdb61563aa144e0cdfe8b0d87b42d03296cd7
Davy <dj.raitt@student.avans.nl>
2020-05-11 19:45:59 +0200

    Checkers board completed :)

--- src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println ("Just a test for the git init :) " );
-        System.out.println ("test2" );
-    }
-}
---
+++ src/networkgame/Board.java
@@ -0,0 +1,137 @@
+package networkgame;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.jfree.fx.FXGraphics2D;
+import org.jfree.fx.ResizableCanvas;
+
+import java.awt.*;
+
+public class Board extends Application {
+	
+	//project data
+	private int width = 1000; // size of the canvas
+	private int height = 1002; // size of the canvas
+	private ResizableCanvas canvas;
+	
+	//fps and fps counter
+	private int x = 0; // x coordinate
+	private int fps = 60; // frames per second , amount of times we want to call update and draw PER SECOND
+	private int timePerUpdate = 1_000_000_000 / fps; // 1 second divided by our fps, now we get our frames per
+	// second
+	private double delta = 0; // delta is the amount of time we have left, untill we call update and draw
+	private long lastTime = System.nanoTime ( ); // gets current computertime in nanoseconds
+	private long timer = 0; // time until we hit 1s, print out how many updates and draw we did
+	private int updates = 0; // int for the fps counter
+	private int lastFPSValue = 0;
+	private boolean colored = false;
+	
+	@Override
+	public void start ( Stage stage ) throws Exception {
+		
+		BorderPane mainPane = new BorderPane ( ); // creating a new borderpane
+		
+		//methods for input
+		canvas = new ResizableCanvas ( g -> draw ( g ) , mainPane );
+		canvas.setFocusTraversable ( true );
+		mainPane.setCenter ( canvas );
+		FXGraphics2D g2d = new FXGraphics2D ( canvas.getGraphicsContext2D ( ) );
+		
+		//calls our init method
+		init ( );
+		
+		new AnimationTimer ( ) { // main game loop, constantly runs update(update) and draw (draw) to show
+			// things on the screen
+			long last = -1;
+			
+			@Override
+			public void handle ( long now ) {
+				now = System.nanoTime ( );
+				delta += ( now - lastTime ) / timePerUpdate; // delta variable tells the computer when and
+				// when not to call update and draw
+				timer += now - lastTime; // adding the amount of time which has passed, sinds we
+				// update/drawed
+				lastTime = now;
+				if ( last == -1 )
+				{
+					last = now;
+				}
+				update ( ( now - last ) / 1000000000.0 );
+				last = now;
+				draw ( g2d );
+				updates = updates + 1;
+				delta = delta - 1;
+				
+				if ( timer >= 1000000000 )
+				{ // prints how many updates we did in the last second?
+					updates = 0; // reset the variables
+					timer = 0;  // reset the variables
+					
+				}
+				
+			}
+		}.start ( );
+		
+		stage.setScene ( new Scene ( mainPane , width , height ) );
+		stage.setTitle ( "Checkers" );
+		stage.show ( );
+	}
+	
+	private void update ( double v ) {
+		long now = System.nanoTime ( );
+		long last = -1;
+	}
+	
+	private void draw ( FXGraphics2D graphics ) {
+		graphics.setBackground ( Color.white );
+		
+		//Clearing the screen first
+		graphics.clearRect ( 0 , 0 , this.width , this.height );
+		
+		for ( int i = 0 ; i < 1000 ; i = i + 125 )
+		{
+			if ( colored )
+			{
+				graphics.setColor ( Color.white );
+				colored = false;
+			} else
+			{
+				graphics.setColor ( Color.black );
+				colored = true;
+			}
+			
+			for ( int j = 2 ; j < 1000 ; j = j + 125 )
+			{
+				
+				if ( colored )
+				{
+					graphics.setColor ( Color.white );
+					colored = false;
+				} else
+				{
+					graphics.setColor ( Color.black );
+					colored = true;
+				}
+				
+				graphics.fillRect ( i , j , 125 , 125 );
+				
+			}
+		}
+		
+		//FPS drawen
+		if ( timer >= 1000000000 )
+		{ // prints how many updates we did in the last second?
+			lastFPSValue = updates;
+			
+		}
+		
+		//draws the FPS right corner
+		graphics.drawString ( String.valueOf ( lastFPSValue ) , 975 , 15 );
+		
+		// Stop drawing
+		graphics.dispose ( );
+	}
+}
